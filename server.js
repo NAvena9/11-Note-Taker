89a34556de75cd4f9c44533cd3fe2b81a8544697
NOat <nicolas.cedano.avena@gmail.com>
2021-07-08 01:39:48 -0500

    Refactored routes and consolidated files due to weird error I couldnt debug, I did a small change on the logic

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');  //trying other method while debuging
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path");
+const fs = require("fs");
+const uniqid = require("uniqid"); ///Added for the notes Id
+var db = require("./db/db.json");
+
 
 const app = express(); //With this we tell node we are creating the 'express' server
 const PORT = process.env.PORT || 3009; //setting up the port
@@ -10,10 +13,51 @@ const PORT = process.env.PORT || 3009; //setting up the port
 app.use(express.json());  //es como un default, viene en otros ejemplos
 app.use(express.urlencoded({ extended: true }));  //es como un default, viene en otros ejemplos
 app.use(express.static("public"));
-// require("./routes/apiRoutes")(app);
-app.use('/api', apiRoutes);
-// require("./routes/htmlRoutes")(app);
-app.use('/', htmlRoutes);
 
-//LISTENER
-app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));// This is the Listener, 'starts' the server on the port (default code)
+
+// -------------------------------------------- Routes -------------------------------------------
+
+//HTML Routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+
+
+// API Routes
+app.get("/api/notes", (req,res) => {
+    res.json(db);
+})
+
+app.post("/api/notes", (req, res) => {
+    let data = req.body;
+    data.id = uniqid(); 
+    db.push(data);
+    console.log(db);
+    fs.writeFile("./db/db.json", JSON.stringify(db), err => {
+        if (err) throw err
+        res.json("ErrorP")
+    })
+})
+
+
+//Delete Option
+app.delete("/api/notes/:id", (req, res) => {
+    let IdToDelete = req.params.id;
+    for(i=0; i < db.length; i++){
+        if(IdToDelete == db[i].id){
+            db.splice(i,1);
+        }
+    }
+    fs.writeFile("./db/db.json", JSON.stringify(db), err => {
+        if(err) throw err
+        res.json("ErrorD")
+    })
+})
+
+
+
+// -------------------------------------------- LISTENER -------------------------------------------
+
+// This is the Listener, 'starts' the server on the port (default code)
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
