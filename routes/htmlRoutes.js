47003cee5d6cb8929cce053f59a11b445c171a00
NOat <nicolas.cedano.avena@gmail.com>
2021-07-08 00:43:14 -0500

    replaced FE (the same code)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,8 +4,7 @@ const router = require("express").Router();
 
     //HTML GET Requests
     //each route below shows the usr an HTML page of content
-    module.exports = (app) => {
-    app.get('/notes', (req, res) => {
+    router.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'))
     });
 
@@ -13,10 +12,28 @@ const router = require("express").Router();
     //     res.sendFile(path.join(__dirname, '../public/notes.html'))
     // });
 
-    app.get('*', (req, res) => {
+    router.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'))
     });
 
-}
+    module.exports = router;
 
-    // module.exports = router;
+
+    //    //each route below shows the usr an HTML page of content
+    //    module.exports = (app) => {
+    //     app.get('/notes', (req, res) => {
+    //         res.sendFile(path.join(__dirname, '../public/notes.html'))
+    //     });
+    
+    //     // app.get('/', (req, res) => {
+    //     //     res.sendFile(path.join(__dirname, '../public/notes.html'))
+    //     // });
+    
+    //     app.get('*', (req, res) => {
+    //         res.sendFile(path.join(__dirname, '../public/index.html'))
+    //     });
+    
+    // }
+    
+    //     // module.exports = router;
+    
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');  //trying other method while debuging
-// const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');  //trying other method while debuging
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express(); //With this we tell node we are creating the 'express' server
 const PORT = process.env.PORT || 3009; //setting up the port
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3009; //setting up the port
 //set up the express app to handle data parsing, static and route middleware
 app.use(express.json());  //es como un default, viene en otros ejemplos
 app.use(express.urlencoded({ extended: true }));  //es como un default, viene en otros ejemplos
-// app.use(express.static("public"));
-require("./routes/apiRoutes")(app);
-// app.use('/api', apiRoutes);
-require("./routes/htmlRoutes")(app);
-// app.use('/', htmlRoutes);
+app.use(express.static("public"));
+// require("./routes/apiRoutes")(app);
+app.use('/api', apiRoutes);
+// require("./routes/htmlRoutes")(app);
+app.use('/', htmlRoutes);
 
 //LISTENER
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));// This is the Listener, 'starts' the server on the port (default code)
